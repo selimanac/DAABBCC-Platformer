187a033ca3ca2b5f2617b1355454f8bbe6feeab7
selimanac <selimanac@gmail.com>
2018-02-25 21:47:01 +0300

    0.2

--- scripts/controller.lua
+++ scripts/controller.lua
@@ -4,13 +4,13 @@ M.world = {}
 local world =  M.world
 
 
-function M.addRect(_name, _position,_size,_type)
+function M.addRect(_name, _position,_size,_type, _go_name, _sprite_url)
 	local _id = daabbcc.insertRect(_name, _position.x , _position.y, _size.x, _size.y)
 	local sub_table_name = _type.."s"
 	if world[sub_table_name] == nil then
 		world[sub_table_name] = {}
 	end
-	table.insert(world[sub_table_name],#world[sub_table_name] + 1 , {treeID = _id, position = _position, size = _size, type =_type}  )
+	table.insert(world[sub_table_name],_id , {treeID = _id, position = _position, size = _size, type =_type,name = _go_name, sprite = _sprite_url}  )
 	return _id
 end
 
