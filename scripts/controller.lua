287f60bf2128e68e765da995f27464fa0109fb87
selimanac <selimanac@gmail.com>
2018-03-08 19:11:51 +0300

    Raycast added

--- scripts/controller.lua
+++ scripts/controller.lua
@@ -10,8 +10,10 @@ function M.addRect(_name, _position,_size,_type, _go_name, _sprite_url)
 	if world[sub_table_name] == nil then
 		world[sub_table_name] = {}
 	end
+	
 	table.insert(world[sub_table_name],_id , {treeID = _id, position = _position, size = _size, type =_type,name = _go_name, sprite = _sprite_url}  )
 	return _id
+
 end
 
 function M.updateRect(_name, _id, _position, _size)
