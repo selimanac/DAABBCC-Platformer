5203b54226ecde8c437bf7d3cbb92a88bd0c4346
selimanac <selimanac@gmail.com>
2018-03-16 13:39:10 +0300

    Fix

---
+++ assets/objects/bee.go
@@ -0,0 +1,20 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/assets/atlas/main.atlas\"\n"
+  "default_animation: \"bee\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.7
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
